04323b800ef77200c3aea0fe2096a4d6024cdae2
Ton Huynh Chi <huynhchiton981@gmail.com>
2026-04-07 18:45:31 +0700

    Update solution for image-histogram

--- image-histogram/image-histogram.py
+++ image-histogram/image-histogram.py
@@ -1,10 +1,9 @@
+import numpy as np
 def image_histogram(image):
     """
     Compute the intensity histogram of a grayscale image.
     """
     # Write code here
-    colors = [0] * 256
-    for x in image:
-        for y in x:
-            colors[y] += 1
-    return colors
+    img_array = np.array(image).ravel()
+    hist = np.bincount(img_array, minlength=256)
+    return hist.tolist()
